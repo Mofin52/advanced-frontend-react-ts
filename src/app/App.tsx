123197facaccba347e17fb531d55bc797d0e42af
Alexander Velikorechin <a.velikorechin@gmail.com>
2023-05-07 22:16:35 +0300

    Feature sliced

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,10 +2,10 @@ import React, {Suspense, useContext, useState} from 'react';
 import './styles/index.scss';
 import {Routes, Route, Link} from 'react-router-dom'
 
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classnames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
+import {classNames} from "shared/lib/classnames/classNames";
 
 
 const App = () => {
@@ -18,8 +18,8 @@ const App = () => {
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync/>}/>
-                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/'} element={<MainPage/>}/>
                 </Routes>
             </Suspense>
 
