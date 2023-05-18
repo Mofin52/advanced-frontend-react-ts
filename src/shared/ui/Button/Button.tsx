6731a72713eb917739f9211454e4df95362c6638
Alexander Velikorechin <a.velikorechin@gmail.com>
2023-05-18 13:55:58 +0300

    Storybook

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -3,7 +3,8 @@ import { ButtonHTMLAttributes, FC } from 'react';
 import cls from './Button.module.scss';
 
 export enum ThemeButton {
-    CLEAR = 'clear'
+    CLEAR = 'clear',
+    OUTLINE = 'outline',
 }
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
     className?: string;
@@ -17,7 +18,11 @@ export const Button: FC<ButtonProps> = (props) => {
   } = props;
 
   return (
-    <button type="button" className={classNames(cls.Button, {}, [className, cls[theme]])} {...otherProps}>
+    <button
+      type="button"
+      className={classNames(cls.Button, {}, [className, cls[theme]])}
+      {...otherProps}
+    >
       {children}
     </button>
   );
