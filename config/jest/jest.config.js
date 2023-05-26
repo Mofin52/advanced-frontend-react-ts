28529e2127f20d19827f0018774c5c4d81a068aa
Alexander Velikorechin <a.velikorechin@gmail.com>
2023-05-27 00:39:06 +0300

    reduxtoolkit

--- config/jest/jest.config.js
+++ config/jest/jest.config.js
@@ -50,6 +50,9 @@ module.exports = {
   testMatch: [
     '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
   ],
+  globals: {
+    __IS_DEV__: true,
+  },
   // Indicates whether the coverage information should be collected while executing the test
   // collectCoverage: false,
 
@@ -89,7 +92,6 @@ module.exports = {
   // globalTeardown: undefined,
 
   // A set of global variables that need to be available in all test environments
-  // globals: {},
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
   // maxWorkers: "50%",
