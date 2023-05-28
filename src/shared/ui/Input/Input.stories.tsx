6cc04fa4dd315fe9f7b877c915486be8b6539f8e
Alexander Velikorechin <a.velikorechin@gmail.com>
2023-05-28 14:45:48 +0300

    Style fix

--- src/shared/ui/Input/Input.stories.tsx
+++ src/shared/ui/Input/Input.stories.tsx
@@ -3,6 +3,7 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
+import { OutlineDark } from 'shared/ui/Button/Button.stories';
 import { Input } from './Input';
 
 export default {
