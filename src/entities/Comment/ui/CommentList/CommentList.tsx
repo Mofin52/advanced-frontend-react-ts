daf6899c7f23ffa2165d6703ebaf05aabe5cd7b8
Alexander Velikorechin <a.velikorechin@gmail.com>
2023-07-04 18:39:41 +0300

    Article comments

---
+++ src/entities/Comment/ui/CommentList/CommentList.tsx
@@ -0,0 +1,33 @@
+import { classNames } from 'shared/lib/classnames/classNames';
+import { useTranslation } from 'react-i18next';
+import { memo } from 'react';
+import { Text } from 'shared/ui/Text/Text';
+import { CommentCard } from '../CommentCard/CommentCard';
+import cls from './CommentList.module.scss';
+import { Comment } from '../../model/types/comment';
+
+interface CommentListProps {
+    className?: string;
+    comments?: Comment[];
+    isLoading?: boolean;
+}
+
+export const CommentList = memo(({ className, isLoading, comments }: CommentListProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <div className={classNames(cls.CommentList, {}, [className])}>
+      {
+          comments?.length
+            ? comments.map((comment) => (
+              <CommentCard
+                comment={comment}
+                className={cls.comment}
+                isLoading={isLoading}
+              />
+            ))
+            : <Text text={t('Комментарии отсутствуют')} />
+      }
+    </div>
+  );
+});
--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -3,23 +3,55 @@ import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
 import { ArticleDetails } from 'entities/Article';
 import { useParams } from 'react-router-dom';
+import { Text } from 'shared/ui/Text/Text';
+import { CommentList } from 'entities/Comment/ui/CommentList/CommentList';
+import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import { useDispatch, useSelector } from 'react-redux';
+import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
+import {
+  fetchCommentsByArticleId,
+} from 'pages/ArticleDetailsPage/model/services/fetchCommentsByArticleId/fetchCommentsByArticleId';
+import { articleDetailsCommentsReducer, getArticleComments } from '../../model/slice/articleDetailsCommentsSlice';
 import cls from './ArticleDetailsPage.module.scss';
+import { getArticleCommentsIsLoading } from '../../model/selectors/comments';
 
 interface ArticleDetailsPageProps {
     className?: string
 }
+
+const reducers: ReducersList = {
+  articleDetailsComments: articleDetailsCommentsReducer,
+};
 
 const ArticleDetailsPage = ({ className }: ArticleDetailsPageProps) => {
   const { t } = useTranslation();
 
   const { id } = useParams<{id: string}>();
 
+  const comments = useSelector(getArticleComments.selectAll);
+  const isLoading = useSelector(getArticleCommentsIsLoading);
+
+  const dispatch = useDispatch();
+
+  useInitialEffect(() => {
+    dispatch(fetchCommentsByArticleId(id));
+  });
+
   if (!id) {
     return <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>{t('Статья не найдена')}</div>;
   }
 
   return (
-    <div className={classNames(cls.ArticleDetailsPage, {}, [className])}><ArticleDetails id={id} /></div>
+    <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
+      <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+        <ArticleDetails id={id} />
+        <Text title={t('Комментарии')} className={cls.commentTitle} />
+        <CommentList
+          isLoading={isLoading}
+          comments={comments}
+        />
+      </div>
+    </DynamicModuleLoader>
   );
 };
 
