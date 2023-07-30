59d58cd4f5aed713555b54261ac6a507029603c7
Alexander Velikorechin <a.velikorechin@gmail.com>
2023-07-30 23:32:19 +0300

    fix header aquisition

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -7,3 +7,10 @@ export const $api = axios.create({
     authorization: localStorage.getItem(USER_LOCAL_STORAGE_KEY) || '',
   },
 });
+
+$api.interceptors.request.use((config) => {
+  if (config.headers) {
+    config.headers.authorization = localStorage.getItem(USER_LOCAL_STORAGE_KEY) || '';
+  }
+  return config;
+});
