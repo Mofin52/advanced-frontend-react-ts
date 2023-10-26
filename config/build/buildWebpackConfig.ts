f0c13e6e751cbbd06029eae38d74034ab2e47c56
Alexander Velikorechin <a.velikorechin@gmail.com>
2023-10-26 11:07:22 +0300

    Optimize loaders

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -22,7 +22,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
     module: {
       rules: buildLoaders(options),
     },
-    devtool: isDev ? 'inline-source-map' : undefined,
+    devtool: isDev ? 'eval-cheap-module-source-map' : undefined,
     devServer: isDev ? buildDevServer(options) : undefined,
   };
 }
